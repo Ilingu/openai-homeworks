b711605d362972a19160eaa0f994f576c4b0107e
Ilingu <thetitouoff@gmail.com>
2022-04-16 12:02:41 +0200

    [V1-beta.1]: Fix 1 (Vercel)

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import vercel from "@sveltejs/adapter-vercel";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
